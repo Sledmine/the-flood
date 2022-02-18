e21b1c7a6e5cce9f02ccbd4965a5bce9fb888ed3
Sledmine <40512223+Sledmine@users.noreply.github.com>
2022-02-17 22:32:52 -0700

    add fbxtojms script

---
+++ lua/scripts/fbxtojms.lua
@@ -0,0 +1,20 @@
+local fs = require "lua.scripts.modules.fs"
+
+local inputPath = arg[1]
+local fbxPath = inputPath .. "/fbx"
+local jmsPath = inputPath .. "/models"
+
+for nameFbx, entry in fs.dir(fbxPath) do
+    if not nameFbx then
+        print("error: ", entry)
+        break
+    end
+    local nameJms = nameFbx:gsub(".fbx", ".jms"):gsub(".FBX", ".jms")
+    local command = ("wine hek/harvest_h1a_tool fbx-to-jms \"$input\" \"$output\""):gsub("%$(%w+)",
+                                                                                         {
+        input = entry:path(),
+        output = jmsPath .. "/" .. nameJms
+    })
+    os.execute(command)
+end
+fs.remove("gamestate.txt")
