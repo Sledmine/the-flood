d0ec7f04556c1144d3bab5acc05ca6868c5f9273
Sledmine <40512223+Sledmine@users.noreply.github.com>
2023-02-11 03:14:48 -0600

    show version text in game

--- lua/multiplayer/treason.lua
+++ lua/multiplayer/treason.lua
@@ -12,10 +12,10 @@ dynamicCross = require "multiplayer.features.dynamicCross"
 tagClasses = blam.tagClasses
 objectClasses = blam.objectClasses
 
---function OnLoad()
-    --console_out("it's working")
+-- function OnLoad()
+-- console_out("it's working")
 --    fontOverride.onLoad()
---end
+-- end
 
 function OnTick()
     dynamicCross.dynamicReticles()
@@ -24,10 +24,24 @@ function OnTick()
     gameplay.hudExtensions()
     gameplay.regenerateHealth()
     gameplay.hudUpgrades()
-    --gameplay.pingObjectives()
-    --meleeScreen()
+    -- gameplay.pingObjectives()
+    -- meleeScreen()
+end
+
+function OnFrame()
+    local isPlayerOnMenu = read_byte(blam.addressList.gameOnMenus) == 0
+    if isPlayerOnMenu then
+        return
+    end
+    local font = "small"
+    local align = "center"
+    local bounds = {left = 0, top = 460, right = 640, bottom = 480}
+    local textColor = {1.0, 0.45, 0.72, 1.0}
+    draw_text("thefloodmp-2.1.0", bounds.left, bounds.top, bounds.right, bounds.bottom, font, align,
+              table.unpack(textColor))
 end
 
 set_callback("tick", "OnTick")
+set_callback("preframe", "OnFrame")
 
---OnLoad()
+-- OnLoad()
