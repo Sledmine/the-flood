e70b8df922a898b97e26e6078468720db7b252f7
markmcfuzz <magecape10@gmail.com>
2023-09-24 21:54:04 -0600

    package version appears when you pause the game

--- lua/multiplayer/the_flood_mp_client.lua
+++ lua/multiplayer/the_flood_mp_client.lua
@@ -38,7 +38,7 @@ function OnFrame()
     local align = "center"
     local bounds = {left = 0, top = 460, right = 640, bottom = 480}
     local textColor = {1.0, 0.45, 0.72, 1.0}
-    draw_text("thefloodmp-4.4.0", bounds.left, bounds.top, bounds.right, bounds.bottom, font, align,
+    draw_text("thefloodmp-4.5.0", bounds.left, bounds.top, bounds.right, bounds.bottom, font, align,
               table.unpack(textColor))
 end
 
