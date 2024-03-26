763a43cc11d946b57035cd6413356990b3788dfd
markmcfuzz <magecape10@gmail.com>
2024-03-26 02:50:42 -0600

    updated ads script

--- lua/the_flood/gameplay_core/aimingDownSights.lua
+++ lua/the_flood/gameplay_core/aimingDownSights.lua
@@ -3,6 +3,16 @@ local const = require "the_flood.constants"
 
 local aimingDownSights = {}
 
+--- Attempt to play a sound given tag path and optionally a gain number
+function aimingDownSights.playSound(tagPath, gain)
+    local player = blam.player(get_player())
+    if (player) then
+        local playSoundCommand = const.hsc.playSound:format(tagPath, player.index, gain or 1.0)
+        execute_script(playSoundCommand)
+    end
+end
+
+-- ADS system for some weapons
 function aimingDownSights.adsSystem()
     local player = blam.biped(get_dynamic_player())
     if player then
@@ -12,12 +22,14 @@ function aimingDownSights.adsSystem()
         if player.actionKey then -- and objectType == 2 and currentWeapon then
             local tag = blam.getTag(player.tagId)
             assert(tag, "Tag not found")
-            local fieldOfView = read_dword(tag.data + 0x1A0)
+            local fieldOfView = math.deg(read_float(tag.data + 0x1A0))
             console_out("FOV: " .. fieldOfView)
-            if fieldOfView == 70 then
-                write_dword(tag.data + 0x1A0, 50)
+            if fieldOfView > 60 then
+                write_float(tag.data + 0x1A0, math.rad(55))
+                aimingDownSights.playSound(const.sounds.humanRifleZoomIn, 5)
             else
-                write_dword(tag.data + 0x1A0, 70)
+                write_float(tag.data + 0x1A0, math.rad(70))
+                aimingDownSights.playSound(const.sounds.humanRifleZoomOut, 5)
             end
         end
     end
