a1dcef53069689cb3130cb982635125352f9ca65
Sledmine <40512223+Sledmine@users.noreply.github.com>
2024-03-26 01:20:11 -0600

    attempt to supply markus weird needs

--- lua/the_flood/gameplay_core/aimingDownSights.lua
+++ lua/the_flood/gameplay_core/aimingDownSights.lua
@@ -5,14 +5,21 @@ local aimingDownSights = {}
 
 function aimingDownSights.adsSystem()
     local player = blam.biped(get_dynamic_player())
--- local currentWeapon = read_word(player + 0x118) -- current weapon
--- local objectType = read_word(player + 0xB4) -- (0 = Biped) (1 = Vehicle) (2 = Weapon) (3 = Equipment) (4 = Garbage) (5 = Projectile) (6 = Scenery) (7 = Machine) (8 = Control) (9 = Light Fixture) (10 = Placeholder) (11 = Sound Scenery)
-        --console_out("ADS Working")
-        if player.actionKey then-- and objectType == 2 and currentWeapon then
-        local fieldOfView = read_word(player + 0x1A0) -- field of view from biped
-        if fieldOfView == 70 then
-          fieldOfView = 50
-       end
+    if player then
+        -- local currentWeapon = read_word(player + 0x118) -- current weapon
+        -- local objectType = read_word(player + 0xB4) -- (0 = Biped) (1 = Vehicle) (2 = Weapon) (3 = Equipment) (4 = Garbage) (5 = Projectile) (6 = Scenery) (7 = Machine) (8 = Control) (9 = Light Fixture) (10 = Placeholder) (11 = Sound Scenery)
+        -- console_out("ADS Working")
+        if player.actionKey then -- and objectType == 2 and currentWeapon then
+            local tag = blam.getTag(player.tagId)
+            assert(tag, "Tag not found")
+            local fieldOfView = read_dword(tag.data + 0x1A0)
+            console_out("FOV: " .. fieldOfView)
+            if fieldOfView == 70 then
+                write_dword(tag.data + 0x1A0, 50)
+            else
+                write_dword(tag.data + 0x1A0, 70)
+            end
+        end
     end
 end
 
