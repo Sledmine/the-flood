d1ab08a3e21bdbbd5e033de675c3ea0be0f94115
markmcfuzz <magecape10@gmail.com>
2024-03-27 00:38:27 -0600

    updated ads system

--- lua/the_flood/gameplay_core/aimingDownSights.lua
+++ lua/the_flood/gameplay_core/aimingDownSights.lua
@@ -14,9 +14,11 @@ end
 
 -- ADS system for some weapons
 function aimingDownSights.adsSystem()
-    local player = blam.biped(get_dynamic_player())
+    local player = blam.biped(get_dynamic_player ())
+    --local object = get_object(read_dword(player + 0x118))
+    --local readyTimer = read_word(player + 0x23A)
     if player then
-        -- local currentWeapon = read_word(player + 0x118) -- current weapon
+        --  -- current weapon
         -- local objectType = read_word(player + 0xB4) -- (0 = Biped) (1 = Vehicle) (2 = Weapon) (3 = Equipment) (4 = Garbage) (5 = Projectile) (6 = Scenery) (7 = Machine) (8 = Control) (9 = Light Fixture) (10 = Placeholder) (11 = Sound Scenery)
         -- console_out("ADS Working")
         if player.actionKey then -- and objectType == 2 and currentWeapon then
