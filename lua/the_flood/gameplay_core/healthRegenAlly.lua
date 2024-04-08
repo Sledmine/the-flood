d25a0c0c3a503a4d1e6448974360a78d8c352afb
markmcfuzz <magecape10@gmail.com>
2024-04-08 17:11:18 -0600

    updated healthRegenAlly

--- lua/the_flood/gameplay_core/healthRegenAlly.lua
+++ lua/the_flood/gameplay_core/healthRegenAlly.lua
@@ -5,9 +5,12 @@ local healthRegenAlly = {}
 
 --- Regenerate player health on low shield using game ticks on singleplayer
 function healthRegenAlly.regenerateHealth()
-    local ally = (const.biped.odstAlly)
-    if (ally) then
-        -- Fix muted audio shield sync
+    local allyId = const.biped.odstAlly.id
+    local ally = blam.biped(get_object(allyId))
+    local player = blam.biped(get_dynamic_player())
+    if (ally) and (player) then
+        local tag = blam.getTag(ally.tagId)
+        assert(tag, "Tag not found")
         if (server_type == "local") then
             if (ally.health <= 0) then
                 ally.health = 0.000000001
