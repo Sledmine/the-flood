7f0dddeda56ad5afec3977c91310577a4e7b92a7
markmcfuzz <magecape10@gmail.com>
2024-04-09 01:02:19 -0600

    updated ally health regen

--- lua/the_flood/gameplay_core/healthRegenAlly.lua
+++ lua/the_flood/gameplay_core/healthRegenAlly.lua
@@ -3,25 +3,20 @@ local const = require "the_flood.constants"
 
 local healthRegenAlly = {}
 
---- Regenerate player health on low shield using game ticks on singleplayer
+--- Regenerate a designated biped health on low shield using game ticks on singleplayer
 function healthRegenAlly.regenerateHealth()
-    local allyId = const.biped.odstAlly.id
-    local ally = blam.biped(get_object(allyId))
-    local player = blam.biped(get_dynamic_player())
-    if (ally) and (player) then
-        local tag = blam.getTag(ally.tagId)
-        assert(tag, "Tag not found")
-        if (server_type == "local") then
-            if (ally.health <= 0) then
-                ally.health = 0.000000001
-            end
-        end
-        if (ally.health < 1 and ally.shield >= 1) then
-            local newAllyHealth = ally.health + const.healthRegenerationAmount
-            if (newAllyHealth > 1) then
-                ally.health = 1
-            else
-                ally.health = newAllyHealth
+    for objectId, index in pairs(blam.getObjects()) do
+        local object = blam.getObject(objectId)
+        if object then
+            local tagId = object.tagId
+            if tagId == const.biped.odstAllyTag.id then
+                --console_out("juasjuasjuas")
+                if object.health < 1 and object.shield > 0.95 then
+                    object.health = object.health + const.healthRegenAiAmount
+                    if object.health > 1 then
+                        object.health = 1
+                    end
+                end
             end
         end
     end
