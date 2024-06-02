ed84766cea4b4a9cab87b9f997d279daf47cbb2c
markmcfuzz <magecape10@gmail.com>
2024-06-01 18:12:45 -0600

    implemented better method

--- lua/the_flood/gameplay_core/healthRegen.lua
+++ lua/the_flood/gameplay_core/healthRegen.lua
@@ -3,10 +3,14 @@ local const = require "the_flood.constants"
 
 local healthRegen = {}
 
+local isGameClient = function ()
+    return blam.isGameDedicated() or blam.isGameHost()
+end
+
 --- Attempt to play a sound given tag path and optionally a gain number
 function healthRegen.playSound(tagPath, gain)
     local player = blam.player(get_player())
-    if (player) then
+    if player then
         local playSoundCommand = const.hsc.playSound:format(tagPath, player.index, gain or 1.0)
         execute_script(playSoundCommand)
     end
@@ -15,25 +19,28 @@ end
 --- Regenerate players health on low shield using game ticks
 ---@param playerIndex? number
 function healthRegen.regenerateHealth(playerIndex)
-    if (server_type == "sapp" or server_type == "local" or server_type == "dedicated") then
+    if blam.isGameSAPP() or isGameClient() then
         local player
-        if (playerIndex) then
+        if playerIndex then
             player = blam.biped(get_dynamic_player(playerIndex))
         else
             player = blam.biped(get_dynamic_player())
         end
-        if (player) then
+        if player then
             -- Fix muted audio shield sync
-            if (server_type == "local") then
-                if (player.health <= 0) then
+            -- TODO Not sure this will work as expected
+            if isGameClient() then
+                if player.health <= 0 then
                     player.health = 0.000000001
                 end
             end
-            if (player.health < 1 and player.shield >= 0.98) then
+            if player.health < 1 and player.shield >= 0.98 then
                 local newPlayerHealth = player.health + const.healthRegenAiAmount
-                if (newPlayerHealth > 1) then
+                if newPlayerHealth > 1 then
                     player.health = 1
-                    healthRegen.playSound(const.sounds.humanRifleZoomIn, 5)
+                    if isGameClient() then
+                        healthRegen.playSound(const.sounds.humanRifleZoomIn, 5)
+                    end
                 else
                     player.health = newPlayerHealth
                 end
