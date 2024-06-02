79aeaa339f234922f1bd4fe217c6dc590bd0a77a
markmcfuzz <magecape10@gmail.com>
2024-06-01 18:13:07 -0600

    added colors

--- lua/the_flood/constants.lua
+++ lua/the_flood/constants.lua
@@ -5,6 +5,71 @@ local findTag = blam.findTag
 
 local constants = {}
 
+constants.color = {
+    ---SULFUR---
+    cookies_and_cream = "#d9e1ac",
+    misty_moss = "#b3b873",
+    oxley = "#74a071",
+    mustard_green = "#62703c",
+    soldier__green = "#505929",
+    ---SILVER---
+    opal_white = "#a4c0c1", "best option",
+    weldon_blue = "#83a1a6",
+    steel_teal = "#578a92", "best option",
+    dark_electric_blue ="#507178",
+    ---BLUE---
+    jordi_blue = "#8bb2ff",
+    catalina = "#142d75",
+    ---GREEN---
+    russian_green = "#77935d",
+    japanese_laurel = "#3b701d",
+    lincoln_green = "#225004",
+    ---PURPLE---
+    waterloo = "#82839a",
+    indigo = "#261e7b",
+    ---GOLDEN---
+    sahara = "#b1af14",
+    verdun = "#5c4d00",
+    ---RED---
+    copper ="#9d646b",
+    dark_tan = "#6a0d0f",
+    tamarind = "#341722",
+    ---ORANGE---
+    bell = "#e38e15",
+    fire = "#a74500",
+    beech = "#7e3000",
+    ---BLACK---
+    space ="#273132",
+    black = "#000000",
+    ---WHITE---
+    white = "#FFFFFF",
+    mist = "##a5c5cb",
+    ---CYANOTIC---
+    mint = "#96edca",
+    mulled_wine = "#473e5a",
+    ---NIGHTFALL---
+    keppel ="#37979d",
+    eden ="#125962",
+    elephant = "#0c3739",
+    ---MATRIX---
+    blue_chill = "#147d97",
+    meadow = "#1ca569",
+    ---SUNSHINE---
+    jellyfish = "#5bc4b2",
+    camel = "#c2a54d",
+    ---CHIMERA---
+    blueberry = "#3c379a",
+    valentine = "#e35a58",
+    ---IRIDESCENT---
+    rose = "#fa51aa",
+    bush = "#633da9",
+    tealish = "#a6b5eb",
+    ---COALESCENCE---
+    american_blue = "#3e2f76",
+    crayola = "#eb1e49",
+    berry = "#a01c4b",
+}
+
 -- Constant core values
 constants.localPlayerAddress = 0x815918
 
