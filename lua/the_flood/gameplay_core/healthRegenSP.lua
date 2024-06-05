7d1e9aeceae52ec37509d3952f74b08380d47718
markmcfuzz <magecape10@gmail.com>
2024-06-04 22:16:38 -0600

    health regen off on vehicles

--- lua/the_flood/gameplay_core/healthRegenSP.lua
+++ lua/the_flood/gameplay_core/healthRegenSP.lua
@@ -6,16 +6,16 @@ local healthRegen = {}
 --- Regenerate player health on low shield using game ticks on singleplayer
 function healthRegen.regenerateHealth()
     local player = blam.biped(get_dynamic_player())
-    if (player) then
+    if player then
         -- Fix muted audio shield sync
-        if (server_type == "local") then
-            if (player.health <= 0) then
+        if server_type == "local" then
+            if player.health <= 0 then
                 player.health = 0.000000001
             end
         end
-        if (player.health < 1 and player.shield >= 0.98) then
+        if player.health < 1 and player.shield >= 0.98 and blam.isNull(player.vehicleObjectId) then
             local newPlayerHealth = player.health + const.healthRegenerationAmount
-            if (newPlayerHealth > 1) then
+            if newPlayerHealth > 1 then
                 player.health = 1
             else
                 player.health = newPlayerHealth
